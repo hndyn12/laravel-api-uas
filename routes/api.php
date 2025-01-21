32757eea1c2282a4ed5d4071c054fcf0f165f86e
Sri Handayani <handayanisri8272@gmail.com>
2025-01-22 00:42:00 +0700

    Update route api.php
    update route api.php 'route users, grade, student, saving'

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\GradeController;
 use App\Http\Controllers\Api\SavingController;
 use App\Http\Controllers\Api\StudentController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
+
+    Route::resource('users', UserController::class);
+    Route::resource('grade', GradeController::class);
+    Route::resource('student', StudentController::class);
+    Route::resource('saving', SavingController::class);
 
     Route::get('/test', function () {
         return view('welcome');
